6a50256ba6a19a20e9681d88a4fd53210058e96a
DESKTOP-GIQGHVN\Yusuf Bayrak <yusufbayrak3429@gmail.com>
2022-11-20 14:08:47 +0300

    Initial commit

---
+++ Assets/Scripts/MarketManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MarketManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+
+        if (InventoryManager.Instance.inventoryLists[0][InventoryManager.Instance.FindItemInInventory(InventoryLocation.player, 10008)].itemQuantity > 5 && Input.GetKeyDown(KeyCode.Z))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                InventoryManager.Instance.RemoveItem(InventoryLocation.player, 10008);
+            }
+
+            InventoryManager.Instance.AddItem(InventoryLocation.player, 10001);
+        }
+
+
+    }
+
+    public void buyItem(int code)
+    {
+        if (InventoryManager.Instance.inventoryLists[0][InventoryManager.Instance.FindItemInInventory(InventoryLocation.player, 10008)].itemQuantity > 5)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                InventoryManager.Instance.RemoveItem(InventoryLocation.player, 10008);
+            }
+
+            InventoryManager.Instance.AddItem(InventoryLocation.player, code);
+        }
+    }
+}
